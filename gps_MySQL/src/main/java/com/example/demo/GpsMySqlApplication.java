865b59db0a9155515c7df86327c137db0a66dadf
Srirammaswamy <32225265+Srirammaswamy@users.noreply.github.com>
2018-08-19 22:25:54 +0530

    Add files via upload

---
+++ gps_MySQL/src/main/java/com/example/demo/GpsMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+//import com.example.mysql.*;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsMySqlApplication.class, args);
+	}
+}
